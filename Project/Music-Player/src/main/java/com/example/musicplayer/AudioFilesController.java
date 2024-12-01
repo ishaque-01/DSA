9bf996b2aee8d3d25b90d871634ad9ecca26d338
ishaque-01 <muhammadishaquememon3@gmail.com>
2024-12-01 14:55:16 +0500

    add playlist creation functionality

--- Project/Music-Player/src/main/java/com/example/musicplayer/AudioFilesController.java
+++ Project/Music-Player/src/main/java/com/example/musicplayer/AudioFilesController.java
@@ -1,7 +1,6 @@
 package com.example.musicplayer;
 
 import javafx.animation.PauseTransition;
-import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -14,7 +13,6 @@ import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
-import javax.swing.JOptionPane;
 import java.io.*;
 import java.net.URL;
 import java.util.LinkedList;
@@ -34,6 +32,7 @@ public class AudioFilesController implements Initializable {
 
     private LinkedList<File> list = new LinkedList<>();
 
+    private String playlistPath = "Playlists/";
     private Stage stage;
     private Scene scene;
     private Parent root;
@@ -71,6 +70,9 @@ public class AudioFilesController implements Initializable {
         createBtn.setFocusTraversable(false);
         playListName.setFocusTraversable(false);
         playlistStatus.setFocusTraversable(false);
+        addPlaylist.setFocusTraversable(false);
+
+        addPlaylist.setDisable(true);
     }
 
 
@@ -88,16 +90,17 @@ public class AudioFilesController implements Initializable {
             modes.getItems().add("No Playlists Available");
             modes.getSelectionModel().selectFirst();
         }
+        addPlaylist.setDisable(false);
     }
 
     public void getPlaylist(ActionEvent e) throws FileNotFoundException {
-        addPlaylist.setDisable(false);
         list.clear();
         filesList.setText("");
         if (!modes.getValue().equalsIgnoreCase("No Playlist Available!") && !modes.getValue().equalsIgnoreCase("Playlists Available")) {
-            String path = "Playlists/" + modes.getValue();
+            String path = playlistPath + modes.getValue();
             File playlist = new File(path);
             if (playlist.exists()) {
+                addPlaylist.setDisable(false);
                 Scanner scan = new Scanner(playlist);
                 StringBuilder sb = new StringBuilder();
                 int count = 1;
@@ -112,35 +115,20 @@ public class AudioFilesController implements Initializable {
                 }
             }
         } else {
-            System.out.println("In Else Part");
-            System.out.println("list.isEmpty(): " + list.isEmpty());
-            printOnTextArea(list);
+            addPlaylist.setDisable(true);
         }
-
     }
 
     // -------------------------------In Process------------------------------------------------
     // Add Button in Process
 
     public void addInPlaylist(ActionEvent e) throws IOException {
-//        String playlistName = modes.getValue();
-//        File playlist = new File("Playlists/" + playlistName);
-//        Scanner scan = new Scanner(playlist);
-//        String currFile = "";
-//        while(scan.hasNextLine()) {
-//            currFile = scan.nextLine();
-//        }
-//        System.out.println("currFile: " + currFile);
-//        int idx = list.indexOf(new File(currFile));
-//        System.out.println("idx: " + idx);
-//        PrintWriter writer = new PrintWriter(playlist);
-//        for(File f:list) {
-//            System.out.println("idx: " + list.indexOf(f));
-//            if(idx < list.indexOf(f)) {
-//                writer.println(f.getAbsolutePath());
-//            }
-//        }
-//        writer.close();
+        String currPlaylistPath = playlistPath + modes.getValue();
+        FileWriter writer = new FileWriter(currPlaylistPath);
+        for (File f : list) {
+            writer.write(f.getAbsolutePath() + "\n");
+        }
+        writer.close();
     }
 
     // Create Button in process
@@ -151,50 +139,27 @@ public class AudioFilesController implements Initializable {
             showErrorMessage(playlistName, filesName);
             return;
         }
-        String path = "Playlists/" + playlistName;
+        String path = playlistPath + playlistName;
         File playlist = new File(path);
         if (playlist.exists()) {
-            boolean checking = checkList(playlist);
-            if (checking) {
-                int answer = JOptionPane.showConfirmDialog(null, "Do you want to update playlist?", "Playlist Update Confirmation", JOptionPane.YES_NO_OPTION);
-                if (answer == JOptionPane.YES_OPTION) {
+            playlistStatus.setText("Use add button to add new files!");
+        } else {
+            try {
+                if (playlist.createNewFile()) {
                     writeInPlaylist(playlist);
-                    playlistStatus.setText("Playlist Updated!");
+                    playlistStatus.setText("Playlist Successfully Created!");
                 }
-            } else {
-                playlistStatus.setText("Nothing to add in playlist!");
+            } catch (IOException io) {
+                System.err.println("Error in creating playlist");
             }
-
         }
-        try {
-            if (playlist.createNewFile()) {
-                writeInPlaylist(playlist);
-                playlistStatus.setText("Playlist Successfully Created!");
-            }
-        } catch (IOException io) {
-            System.err.println("Error in creating playlist");
-        }
+//        updateMode();
         PauseTransition delay = new PauseTransition(Duration.seconds(2));
         delay.setOnFinished(event -> {
+            System.out.println("in delay");
             playlistStatus.setText("");
             playListName.setText("");
         });
-    }
-
-    public boolean checkList(File playlist) {
-
-        try {
-            Scanner scan = new Scanner(playlist);
-            int count = 0;
-            while (scan.hasNextLine()) {
-                count++;
-            }
-            scan.close();
-            if (count < list.size()) return true;
-        } catch (IOException e) {
-            System.err.println("Error in reading playlist!");
-        }
-        return false;
     }
 
     public void writeInPlaylist(File playlist) {
